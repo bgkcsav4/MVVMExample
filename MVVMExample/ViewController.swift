d993a68f50f22effb448d4d13b2e2644396c8646
Latsa <bgkcsavvy@gmail.com>
2021-12-11 19:10:51 +0400

    Initial commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Mariam Latsabidze on 11.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
